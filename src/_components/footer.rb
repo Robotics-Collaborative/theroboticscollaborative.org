36d897ed9acb51aa9a27e3bcd8d5324beef3b3ee
Duncan Miller <1707425+duncantmiller@users.noreply.github.com>
2025-09-01 22:18:32 -0700

    adding homepage image

--- src/_components/footer.rb
+++ src/_components/footer.rb
@@ -14,4 +14,14 @@ class Footer < Bridgetown::Component
       "Rose City Robotics"
     end
   end
+
+  def navigation_items
+    [
+      {name: "Home", href: "/"},
+      {name: "FIRST Robotics", href: "/first_robotics"},
+      {name: "Events", href: "https://lu.ma/wayo5yb9"},
+      {name: "Contact", href: "/contact"},
+      {name: "Rose City Robotics", href: "https://rosecityrobotics.com"}
+    ]
+  end
 end
